130beb678e1622977038394a901062163f9838c5
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-01-05 00:03:02 +0400

    who's there?

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('btn');
-  const p = document.getElementById('p');
-  btn.textContent = 'Wanna colors?';
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const footer = document.querySelector('footer');
+
+  header.textContent = 'Сolor combinations';
+
+  const button = document.createElement('button');
+  button.textContent = 'Wanna colors?';
+  main.append(button);
 
   let changeColors = function() {
     let newColor = '#' + Math.random().toString(16).slice(3, 9);
     let newColorReversed = '#' + (Math.random().toString(16).slice(3, 9)).split().reverse().join();
-    let bodyHexName = `This bg-color is ${newColorReversed}`;
-    let btnHexName = `This bg-color is ${newColor}`;
+    let bodyHexName = `This color is ${newColorReversed}`;
+    let btnHexName = `This color is ${newColor}`;
 
     document.body.style.backgroundColor = newColorReversed;
-    btn.style.backgroundColor = newColor;
-    btn.textContent = btnHexName;
-    p.textContent = bodyHexName;
+    button.style.backgroundColor = newColor;
+    button.textContent = btnHexName;
+    footer.textContent = bodyHexName;
   };
 
-  btn.addEventListener('click', changeColors)
+  button.addEventListener('click', changeColors);
 })
